3741ed974000efecaa782754a773feca1cea31bf
Ajayp007 <156168895+Ajayp007@users.noreply.github.com>
2024-03-04 10:42:33 +0530

    "new"

--- lib/screen/declaration/declaration_screen.dart
+++ lib/screen/declaration/declaration_screen.dart
@@ -10,6 +10,145 @@ class DeclarationScreen extends StatefulWidget {
 class _DeclarationScreenState extends State<DeclarationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigoAccent,
+        title: const Text(
+          "Reference",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey.shade200,
+      body: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Declaration",
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        const Icon(
+                          Icons.adjust_rounded,
+                          size: 50,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: ("Description"),
+                              hintStyle: TextStyle(
+                                  color: Colors.black12, fontSize: 22),
+                              constraints: BoxConstraints(maxHeight: 55),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10)),
+                          textInputAction: TextInputAction.next,
+                        ),
+                        const SizedBox(height: 10),
+                        const Divider(color: Colors.black),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Date",
+                              style: TextStyle(
+                                  color: Colors.black26,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "Place(Interview Venue/City)",
+                              style: TextStyle(
+                                  color: Colors.black26,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: ("DD/MM/YYYY"),
+                                constraints: BoxConstraints(
+                                    maxHeight: 40, maxWidth: 125),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                hintStyle: TextStyle(
+                                    color: Colors.black12, fontSize: 16),
+                              ),
+                              textInputAction: TextInputAction.done,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: ("Junagadh"),
+                                constraints: BoxConstraints(
+                                    maxHeight: 40, maxWidth: 125),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                hintStyle: TextStyle(
+                                    color: Colors.black12, fontSize: 16),
+                              ),
+                              textInputAction: TextInputAction.done,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Center(
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              height: 40,
+                              width: 100,
+                              color: Colors.indigoAccent,
+                              child: const Center(
+                                child: Text(
+                                  "Save",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
